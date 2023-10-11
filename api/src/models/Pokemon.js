0494dda6c7f3b99ef3374d5f1dd95a0b57adb793
leonardobaranelli <leobaranelli@gmail.com>
2023-10-11 18:10:52 -0300

    feat: Pokemon model

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,45 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define('Pokemon', {
+    id : {
+      type : DataTypes.UUID,
+      primaryKey : true,
+      defaultValue : DataTypes.UUIDV4,
+    },
+    name : {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    health : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    attack : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    defense : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    speed : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },        
+  },
+  {freezeTableName : true, timestamps : false}
+  );
 };
