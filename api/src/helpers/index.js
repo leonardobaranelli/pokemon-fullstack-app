bbe730a97999feb1292cbd3e4301eb6cf880b216
leonardobaranelli <leobaranelli@gmail.com>
2023-10-14 11:19:41 -0300

    feat: isIntegerID helper (and index)

---
+++ api/src/helpers/index.js
@@ -0,0 +1,11 @@
+const errorHandler = require("./errorHandler");
+const infoPokemonAPI = require("./infoPokemonAPI");
+const infoPokemonDB = require("./infoPokemonDB");
+const isIntegerID = require("./isIntegerID");
+
+module.exports = {
+  errorHandler,
+  infoPokemonAPI,
+  infoPokemonDB,  
+  isIntegerID,
+};
