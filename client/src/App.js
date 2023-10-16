c5a47709003aa5f2ca95cb82124b7b97c7cf908c
leonardobaranelli <leobaranelli@gmail.com>
2023-10-16 08:56:51 -0300

    feat: connect react with redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
+    <div className="App">      
     </div>
   );
 }
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+    pokemons: [],        
+};
+
+const rootReducer = (state = initialState, { type, payload }) => {
+    switch(type) {
+      
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
+
+
+
+
+                        
