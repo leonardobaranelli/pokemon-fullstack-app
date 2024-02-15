302743b4449657c500b82f2b045631b9790ba283
leonardobaranelli <leobaranelli@gmail.com>
2024-02-15 19:32:53 -0300

    refactor: db config

--- api/src/db.js
+++ api/src/db.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+   `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,
    {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed            
+      logging: false, // Set to console.log to see the raw SQL queries
+      native: false, // Lets Sequelize know we can use pg-native for ~30% more speed            
    }
 );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+//Read all files from the Models folder, request them, and add them to the modelDefiners array
 fs.readdirSync(path.join(__dirname, '/models'))
    .filter(
       (file) =>
@@ -27,9 +27,10 @@ fs.readdirSync(path.join(__dirname, '/models'))
       modelDefiners.push(require(path.join(__dirname, '/models', file)));
    });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+//Inject the connection (sequelize) to all the models         
 modelDefiners.forEach((model) => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+
+//Capitalize model names, ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
    entry[0][0].toUpperCase() + entry[0].slice(1),
